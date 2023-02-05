ad82c478580f2aa77f2c90fcb9e998ad437f5bc6
Felipe da Silva dos Santos <felipe.031293@gmail.com>
2023-02-05 19:49:45 -0300

    fix: add return and status 200

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ app.use(userAgentMiddleware)
 
 app.get('/api/data', (req, res) => {
   if (req.userAgent.includes('Mobile')) {
-    res.send({ message: 'Mobile device detected' })
-  } else if (req.userAgent.includes('Chrome')) {
-    res.send({ message: 'Web browser detected' })
-  } else {
-    res.send({ message: 'Other device detected' })
+    return res.status(200).json({ message: 'Mobile device detected' })
   }
+  if (req.userAgent.includes('Chrome')) {
+    return res.status(200).json({ message: 'Web browser detected' })
+  }
+  return res.status(200).json({ message: 'Other device detected' })
 })
 
 app.listen(3000, () => {
